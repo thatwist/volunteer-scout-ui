c24e5f481d48466f5452876af64b1462404baf87
Yurii Levantovych <domovoj1@gmail.com>
2022-03-17 14:40:41 +0200

    Display one hit per row, removed image, added Views title, display views and rating if exist

--- src/App.js
+++ src/App.js
@@ -66,9 +66,9 @@ const searchClient = typesenseInstantsearchAdapter.searchClient;
 
 const Hit = ({ hit }) => (
   <article className="hit">
-    <header className="hit-image-container">
-      <img src={hit.image} alt={hit.title} className="hit-image" />
-    </header>
+    {/* <header className="hit-image-container">*/}
+    {/*  <img src={hit.image} alt={hit.title} className="hit-image" />*/}
+    {/* </header>*/}
 
     <div className="hit-info-container">
       <p className="hit-category">{hit.category}</p>
@@ -81,23 +81,30 @@ const Hit = ({ hit }) => (
 
       <footer>
         <p>
-          <span className="hit-em">$</span>{' '}
-          <strong>{formatNumber(hit.views)}</strong>{' '}
-          <span className="hit-em hit-rating">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="8"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fill="#e2a400"
-                fillRule="evenodd"
-                d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
-              />
-            </svg>{' '}
-            {hit.rating}
-          </span>
+          {/* <span className="hit-em">$</span>{' '}*/}
+          {hit.views && (
+            <>
+              {'Views:'}{' '}
+              {hit.views && <strong>{formatNumber(hit.views)}</strong>}{' '}
+            </>
+          )}
+          {hit.rating && (
+            <span className="hit-em hit-rating">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="8"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill="#e2a400"
+                  fillRule="evenodd"
+                  d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
+                />
+              </svg>{' '}
+              {hit.rating}
+            </span>
+          )}
         </p>
       </footer>
     </div>
