e712a13ea43a65e4900efcf2d6e7de5057e0023c
Yurii Levantovych <domovoj1@gmail.com>
2022-03-25 22:51:21 +0200

    fix mobile, add count of found, removed search for channels, text adjusting

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   /* Configure, */
   SearchBox,
   Snippet,
+  Stats,
   /* ToggleRefinement, */
 } from 'react-instantsearch-dom';
 /* import algoliasearch from 'algoliasearch/lite'; */
@@ -169,9 +170,9 @@ const App = props => {
         <p className="header-logo"></p>
 
         <p className="header-title">
-          Знаходимо речі, людей, допомогу у десятках telegram каналів
+          Знаходимо речі, людей, допомогу
+          <br />y десятках telegram каналів
         </p>
-
         <SearchBox
           translations={{
             placeholder: 'наприклад, "спальники"',
@@ -207,193 +208,208 @@ const App = props => {
           /> */}
 
       <main className="container" ref={containerRef}>
-        <div className="container-wrapper">
-          <section
-            className="container-filters"
-            onKeyUp={event => onKeyUp(event)}
-          >
-            <div className="container-header">
-              <h2>Фільтри</h2>
-
-              <div className="clear-filters" data-layout="desktop">
-                <ClearRefinements
-                  translations={{
-                    reset: (
-                      <React.Fragment>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="11"
-                          height="11"
-                          viewBox="0 0 11 11"
-                        >
-                          <g fill="none" fillRule="evenodd" opacity=".4">
-                            <path d="M0 0h11v11H0z" />
-                            <path
-                              fill="#000"
-                              fillRule="nonzero"
-                              d="M8.26 2.75a3.896 3.896 0 1 0 1.102 3.262l.007-.056a.49.49 0 0 1 .485-.456c.253 0 .451.206.437.457 0 0 .012-.109-.006.061a4.813 4.813 0 1 1-1.348-3.887v-.987a.458.458 0 1 1 .917.002v2.062a.459.459 0 0 1-.459.459H7.334a.458.458 0 1 1-.002-.917h.928z"
-                            />
-                          </g>
-                        </svg>
-                        Скасувати
-                      </React.Fragment>
-                    ),
-                  }}
-                />
-              </div>
-
-              <div className="clear-filters" data-layout="mobile">
-                <ResultsNumberMobile />
-              </div>
-            </div>
-
-            <div className="container-body">
-              {/* <Panel header="Дата">*/}
-              {/*  <Dates />*/}
-              {/* </Panel>*/}
-              {/* <Panel header="Category">
-                  <HierarchicalMenu
-                  attributes={[
-                  'hierarchicalCategories.lvl0',
-                  'hierarchicalCategories.lvl1',
-                  ]}
-                  />
-                  </Panel>
-                */}
-              {/* <Panel header="Category">*/}
-              {/*  <RefinementList attribute="category" />*/}
-              {/* </Panel>*/}
-              <Panel header="Канали">
-                <RefinementList
-                  attribute="channel"
-                  searchable={true}
-                  translations={{
-                    placeholder: 'Пошук каналів…',
-                  }}
-                />
-              </Panel>
-
-              {/* <Panel header="Price">
-                  <PriceSlider attribute="price" />
-                  </Panel> */}
-
-              {/* <Panel header="Rebooks">
-                  <ToggleRefinement
-                  attribute="free_shipping"
-                  label="Display only items with free shipping"
-                  value={true}
-                  />
-                  </Panel> */}
-
-              {/* <Panel header="Views">
-                  <Ratings attribute="views" />
-                  </Panel> */}
-            </div>
-          </section>
-
-          <footer className="container-filters-footer" data-layout="mobile">
-            <div className="container-filters-footer-button-wrapper">
-              <ClearFiltersMobile containerRef={containerRef} />
-            </div>
-
-            <div className="container-filters-footer-button-wrapper">
-              <SaveFiltersMobile onClick={closeFilters} />
-            </div>
-          </footer>
+        <div className="container-stats">
+          <Stats
+            translations={{
+              stats(nbHits) {
+                return (
+                  <>
+                    <b>{nbHits.toLocaleString()}</b> результатів знайдено
+                  </>
+                );
+              },
+            }}
+          />
         </div>
+        <div className="container-sidebar-results">
+          <div className="container-wrapper">
+            <section
+              className="container-filters"
+              onKeyUp={event => onKeyUp(event)}
+            >
+              <div className="container-header">
+                <h2>Фільтри</h2>
 
-        <section className="container-results">
-          <header className="container-header container-options">
-            {/* <SortBy*/}
-            {/*  className="container-option"*/}
-            {/*  defaultRefinement="posts"*/}
-            {/*  items={[*/}
-            {/*    {*/}
-            {/*      label: 'Sort by featured',*/}
-            {/*      value: 'posts',*/}
-            {/*    },*/}
-            {/*    {*/}
-            {/*      label: 'Date ascending',*/}
-            {/*      value: 'posts_ts_asc',*/}
-            {/*    },*/}
-            {/*    {*/}
-            {/*      label: 'Date descending',*/}
-            {/*      value: 'posts_ts_desc',*/}
-            {/*    },*/}
-            {/*  ]}*/}
-            {/* />*/}
+                <div className="clear-filters" data-layout="desktop">
+                  <ClearRefinements
+                    translations={{
+                      reset: (
+                        <React.Fragment>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="11"
+                            height="11"
+                            viewBox="0 0 11 11"
+                          >
+                            <g fill="none" fillRule="evenodd" opacity=".4">
+                              <path d="M0 0h11v11H0z" />
+                              <path
+                                fill="#000"
+                                fillRule="nonzero"
+                                d="M8.26 2.75a3.896 3.896 0 1 0 1.102 3.262l.007-.056a.49.49 0 0 1 .485-.456c.253 0 .451.206.437.457 0 0 .012-.109-.006.061a4.813 4.813 0 1 1-1.348-3.887v-.987a.458.458 0 1 1 .917.002v2.062a.459.459 0 0 1-.459.459H7.334a.458.458 0 1 1-.002-.917h.928z"
+                              />
+                            </g>
+                          </svg>
+                          Скасувати
+                        </React.Fragment>
+                      ),
+                    }}
+                  />
+                </div>
 
-            {/* <HitsPerPage*/}
-            {/*  className="container-option"*/}
-            {/*  items={[*/}
-            {/*    {*/}
-            {/*      label: '16 hits per page',*/}
-            {/*      value: 16,*/}
-            {/*    },*/}
-            {/*    {*/}
-            {/*      label: '32 hits per page',*/}
-            {/*      value: 32,*/}
-            {/*    },*/}
-            {/*    {*/}
-            {/*      label: '64 hits per page',*/}
-            {/*      value: 64,*/}
-            {/*    },*/}
-            {/*  ]}*/}
-            {/*  defaultRefinement={16}*/}
-            {/* />*/}
-          </header>
+                <div className="clear-filters" data-layout="mobile">
+                  <ResultsNumberMobile />
+                </div>
+              </div>
 
-          <Hits hitComponent={Hit} />
-          <NoResults />
+              <div className="container-body">
+                {/* <Panel header="Дата">*/}
+                {/*  <Dates />*/}
+                {/* </Panel>*/}
+                {/* <Panel header="Category">
+                    <HierarchicalMenu
+                    attributes={[
+                    'hierarchicalCategories.lvl0',
+                    'hierarchicalCategories.lvl1',
+                    ]}
+                    />
+                    </Panel>
+                  */}
+                {/* <Panel header="Category">*/}
+                {/*  <RefinementList attribute="category" />*/}
+                {/* </Panel>*/}
+                <Panel header="Канали">
+                  <RefinementList
+                    attribute="channel"
+                    // searchable={true}
+                    translations={{
+                      placeholder: 'Пошук каналів…',
+                    }}
+                  />
+                </Panel>
 
-          <footer className="container-footer">
-            <Pagination
-              padding={2}
-              showFirst={false}
-              showLast={false}
-              translations={{
-                previous: (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                  >
-                    <g
-                      fill="none"
-                      fillRule="evenodd"
-                      stroke="#000"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.143"
+                {/* <Panel header="Price">
+                    <PriceSlider attribute="price" />
+                    </Panel> */}
+
+                {/* <Panel header="Rebooks">
+                    <ToggleRefinement
+                    attribute="free_shipping"
+                    label="Display only items with free shipping"
+                    value={true}
+                    />
+                    </Panel> */}
+
+                {/* <Panel header="Views">
+                    <Ratings attribute="views" />
+                    </Panel> */}
+              </div>
+            </section>
+
+            <footer className="container-filters-footer" data-layout="mobile">
+              <div className="container-filters-footer-button-wrapper">
+                <ClearFiltersMobile containerRef={containerRef} />
+              </div>
+
+              <div className="container-filters-footer-button-wrapper">
+                <SaveFiltersMobile onClick={closeFilters} />
+              </div>
+            </footer>
+          </div>
+
+          <section className="container-results">
+            <header className="container-header container-options">
+              {/* <SortBy*/}
+              {/*  className="container-option"*/}
+              {/*  defaultRefinement="posts"*/}
+              {/*  items={[*/}
+              {/*    {*/}
+              {/*      label: 'Sort by featured',*/}
+              {/*      value: 'posts',*/}
+              {/*    },*/}
+              {/*    {*/}
+              {/*      label: 'Date ascending',*/}
+              {/*      value: 'posts_ts_asc',*/}
+              {/*    },*/}
+              {/*    {*/}
+              {/*      label: 'Date descending',*/}
+              {/*      value: 'posts_ts_desc',*/}
+              {/*    },*/}
+              {/*  ]}*/}
+              {/* />*/}
+
+              {/* <HitsPerPage*/}
+              {/*  className="container-option"*/}
+              {/*  items={[*/}
+              {/*    {*/}
+              {/*      label: '16 hits per page',*/}
+              {/*      value: 16,*/}
+              {/*    },*/}
+              {/*    {*/}
+              {/*      label: '32 hits per page',*/}
+              {/*      value: 32,*/}
+              {/*    },*/}
+              {/*    {*/}
+              {/*      label: '64 hits per page',*/}
+              {/*      value: 64,*/}
+              {/*    },*/}
+              {/*  ]}*/}
+              {/*  defaultRefinement={16}*/}
+              {/* />*/}
+            </header>
+
+            <Hits hitComponent={Hit} />
+            <NoResults />
+
+            <footer className="container-footer">
+              <Pagination
+                padding={2}
+                showFirst={false}
+                showLast={false}
+                translations={{
+                  previous: (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
                     >
-                      <path d="M9 5H1M5 9L1 5l4-4" />
-                    </g>
-                  </svg>
-                ),
-                next: (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="10"
-                    height="10"
-                    viewBox="0 0 10 10"
-                  >
-                    <g
-                      fill="none"
-                      fillRule="evenodd"
-                      stroke="#000"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.143"
+                      <g
+                        fill="none"
+                        fillRule="evenodd"
+                        stroke="#000"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.143"
+                      >
+                        <path d="M9 5H1M5 9L1 5l4-4" />
+                      </g>
+                    </svg>
+                  ),
+                  next: (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
                     >
-                      <path d="M1 5h8M5 9l4-4-4-4" />
-                    </g>
-                  </svg>
-                ),
-              }}
-            />
-          </footer>
-        </section>
+                      <g
+                        fill="none"
+                        fillRule="evenodd"
+                        stroke="#000"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="1.143"
+                      >
+                        <path d="M1 5h8M5 9l4-4-4-4" />
+                      </g>
+                    </svg>
+                  ),
+                }}
+              />
+            </footer>
+          </section>
+        </div>
       </main>
 
       <aside data-layout="mobile">
